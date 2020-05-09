c3e168dcf1440b25bb46d96c7a34b0155f866d90
Erik Verduin <everduin94@gmail.com>
2020-05-09 00:40:18 -0500

    adding test cases for string util for building markdown

--- src/app/libraryComponents/md-textarea/md-textarea-util.ts
+++ src/app/libraryComponents/md-textarea/md-textarea-util.ts
@@ -1,21 +1,38 @@
 export enum Token {
-    bold,
-    italics,
-    headingOne,
-    headingTwo,
-    headingThree,
-    headingFour,
-    listItem,
-    blockQuote,
-    table,
-    syntax
+    bold = 'bold',
+    italics = 'italics',
+    headingOne = '#',
+    headingTwo = '##',
+    headingThree = '###',
+    headingFour = '####',
+    listItem = '-',
+    blockQuote = '>',
+    table = 'table',
+    syntax = 'syntax'
 }
 
 
 export class MdTextareaUtil {
 
-    insertToken(text:string, token: Token, startIndex:number, endIndex?:number ): string {
-        return '';
+    static getLineNumber(text:string, startIndex:number): number {
+        return text.substr(0, startIndex).split('\n').length;
+    }
+
+    public static insertToken(text:string, token: Token, startIndex:number, endIndex?:number ): string {
+        const currentLineNumber = this.getLineNumber(text, startIndex);
+        const lines = text.split('\n');
+
+        switch (token) {
+            case Token.headingOne:
+            case Token.headingTwo:
+            case Token.headingThree:
+            case Token.headingFour:
+            case Token.listItem:
+            case Token.blockQuote:
+                lines[currentLineNumber - 1] = `${token} ${lines[currentLineNumber - 1]}`;
+        }
+
+        return lines.join('\n');
     }
 
 
--- src/app/libraryComponents/md-textarea/md-textarea.component.spec.ts
+++ src/app/libraryComponents/md-textarea/md-textarea.component.spec.ts
@@ -1,9 +1,29 @@
+import { MdTextareaUtil, Token } from "./md-textarea-util";
 
+
+const textStub = `Hello\nThis is a test\nThird line`
 
 describe('MdTextareaComponent', () => {
   
 
   it('should create', () => {
     
+    // MdTextareaUtil.insertToken(text:string, token: Token, startIndex:number, endIndex?:number )
+
+    const actual = MdTextareaUtil.insertToken(textStub, Token.headingOne, 10);
+    const expected = `Hello\n# This is a test\nThird line`;
+    expect(actual).toEqual(expected);
+
+    const actual1 = MdTextareaUtil.insertToken(textStub, Token.listItem, 10);
+    const expected1 = `Hello\n- This is a test\nThird line`;
+    expect(actual1).toEqual(expected1);
+
+    const actual2 = MdTextareaUtil.insertToken(textStub, Token.headingThree, textStub.length - 1);
+    const expected2 = `Hello\nThis is a test\n### Third line`;
+    expect(actual2).toEqual(expected2);
+
+    const actual3 = MdTextareaUtil.insertToken(textStub, Token.blockQuote, 0);
+    const expected3 = `> Hello\nThis is a test\nThird line`;
+    expect(actual3).toEqual(expected3);
   });
 });
